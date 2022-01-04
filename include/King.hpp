64aac0cab35250f97458a8eda059f0e7519be5f0
mattia-toffolon <mattia.toffolon@studenti.unipd.it>
2022-01-04 22:18:31 +0100

    Aggiunta di King.hpp a Include

---
+++ include/King.hpp
@@ -0,0 +1,21 @@
+#ifndef KING_HPP
+#define KING_HPP
+
+#include "Piece.hpp"
+
+class King : public Piece{
+
+    public:
+        // checks if the chosen move is valid for this King 
+        // (also in reference to the Board in which it's positioned )
+        bool can_move(std::string& from, std::string& to);
+        // generates and returns a vector contaning all the possible moves that this King can do as strings 
+        // (also in reference to the Board in which it's positioned)
+        std::vector<std::string> get_possible_moves(std::string& from);
+        // writes in the os stream the characther associated with this King
+        std::ostream operator<<(std::ostream& os);
+        // returns the characther associated with this King
+        char to_char();
+};
+
+#endif
--- include/Piece.hpp
+++ include/Piece.hpp
@@ -9,7 +9,7 @@
 
 class Piece {
 
-    private:
+    protected:
         // specifies the color of the Piece (true=WHITE, false=BLACK)
         bool ID;
         // specifies if the Piece is still on the dashboard or if it has been eaten
@@ -21,18 +21,18 @@ class Piece {
         // expressions that identify the colors WHITE and BLACK as bool variables (respectivly true and false)
         static constexpr bool WHITE = true;
         static constexpr bool BLACK = false;
-        // checks if the chosen move is valid for the Piece 
+        // checks if the chosen move is valid for this Piece 
         // (also in reference to the Board in which it's positioned )
         virtual bool can_move(std::string& from, std::string& to) = 0;
-        // generates e returns a vector contaning all the possible moves that the Piece can do as strings 
-        // (also in reference to the Board in which it's positioned )
+        // generates and returns a vector contaning all the possible moves that this Piece can do as strings 
+        // (also in reference to the Board in which it's positioned)
         virtual std::vector<std::string> get_possible_moves(std::string& from) = 0;
-        // writes in the os stream the characther associated with the Piece
+        // writes in the os stream the characther associated with this Piece
         virtual std::ostream operator<<(std::ostream& os) = 0;
         // returns a copy of the Piece's ID (that determines its color)
         bool get_ID();
-        // return the characther associated with the Piece
-        char to_char();
+        // return the characther associated with this Piece
+        virtual char to_char() = 0;
 
 };
 
