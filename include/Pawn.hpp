8e6352b8da635318fe13bc5033ce1d582f88be96
mattia-toffolon <mattia.toffolon@studenti.unipd.it>
2022-01-14 22:04:19 +0100

    Aggiunta en-passant e modifiche a metodi can_move

--- include/Pawn.hpp
+++ include/Pawn.hpp
@@ -7,9 +7,15 @@
 
 class Pawn : public Piece{
 
+    private:
+        // determines if the en-passant can be performed on this Pawn or not
+        bool en_passant;
+
     public:
         // constructs an object "Rook" with the given ID, reference to the Board and initial position
-        Pawn(const bool& ID, Board* b, std::string arg_pos) : Piece(ID, b, arg_pos) {};
+        // (en-passant is set to false when to the object is created because to enable the en-passant condition
+        // the selected Pawn must have moved by to tiles up or down in one move from its starting position)
+        Pawn(const bool& ID, Board* b, std::string arg_pos) : Piece(ID, b, arg_pos) {en_passant=false;};
         // checks if the chosen move is valid for this Pawn 
         // (in reference to the Board in which it's positioned)
         // can trow an exception if the move is illegal or if the match ends
@@ -21,6 +27,10 @@ class Pawn : public Piece{
         std::ostream& operator<<(std::ostream& os);
         // returns the characther associated with this Pawn
         char to_char();
+        // sets en_passant
+        void set_en_passant(bool p);
+        // returns en_passant
+        bool get_en_passant() const;
 };
 
 //#include "../src/Pawn.cpp"
