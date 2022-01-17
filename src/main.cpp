165f45860ed8f26d13693b9915ca34ff4c187d68
Alberto <alberto.guerrini.1@studenti.unipd.it>
2022-01-17 20:10:15 +0100

    x

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,23 @@
 int main(){
     // test
     Board b {Piece::WHITE};
+    std::string from;
+    std::string to;
 
-    std::cout << "\n" << b << std::endl;
+    while(true){
+        std::cout << std::endl <<"Mossa: ";
+        std::cin >> from;
+        std::cin >> to;
+        try
+        {
+            b.move(from, to, Piece::WHITE);
+        }
+        catch(IllegalMoveException& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        std::cout << std::endl << b << std::endl;
+    }
 
     return 0;
 }
